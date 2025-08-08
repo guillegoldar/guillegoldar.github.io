9d0b92fad319ee9f7edc569d7b35f904a73ba8b8
guillegoldar <guillermogoldar@gmail.com>
2025-08-08 17:12:34 -0300

    Bug

--- sauce.js
+++ sauce.js
@@ -35,7 +35,7 @@ let obtenerCotHedera = (Id) => {
       } else {
           alert('Error fetching data Hedera (1):' + xhr.statusText);
       }
-      alert(resultado.attributes);
+      alert(resultado.data);
       let cot = resultado.base_token_price_usd;
       return cot;
     }
