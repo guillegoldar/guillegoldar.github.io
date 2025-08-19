47f8d0bb04f1bf4aaf643cc88683ff98af5ccff5
guillegoldar <guillermogoldar@gmail.com>
2025-08-19 10:08:57 -0300

    Bug

--- sauce.js
+++ sauce.js
@@ -133,7 +133,7 @@ let calcularTotal=() => {
   let cotKeeta = document.getElementById('spCotKeeta').innerText;
   let cotDosa = document.getElementById('spCotDosa').innerText;  
   //let cotZAP = document.getElementById('spCotZAP').innerText;
-  //let cotHert = document.getElementById('spCotHert').innerText;
+  let cotHert = document.getElementById('spCotHert').innerText;
   let total = (tenenciaUSDC * cotUSDC
   				   + tenenciaWBTC * cotWBTC
   				   + tenenciaHBAR * cotHBAR				   
@@ -141,7 +141,7 @@ let calcularTotal=() => {
      			   + tenenciaKeeta * cotKeeta
 				     + tenenciaDosa * cotDosa
 				     //+ tenenciaZAP * cotZAP
-				     //+ tenenciaHert * cotHert
+				     + tenenciaHert * cotHert
 				)
   return total;
 };
